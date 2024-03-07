40fe41b18740bb5450675cf1c88a55d5d1b7cd00
dezcicleto <dordonez@doncicleto.com>
2024-03-06 23:13:49 -0500

    section 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/hereos.module';
 
 import { AppComponent } from './app.component';
+
+
 
 
 @NgModule({
@@ -14,7 +17,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
-    HeroesModule
+    HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
